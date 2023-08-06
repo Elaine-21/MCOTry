1cba90666a9dc4cb20875076502744e8926c7301
Elaine-21 <101459900+Elaine-21@users.noreply.github.com>
2023-08-06 20:22:38 +0800

    Add files via upload

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URL, {
+const url = "mongodb+srv://elainemartin022:vcXcJq891xQPD940@cluster0.x2as0ew.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url, {
     useNewURLParser: true,
     useUnifiedTopology:true,
     family: 4
 })
     .then(() => { console.log('Connected to Mongodb')})
     .catch((err) => { console.log(`Connection failed: ${err}`)})
+
